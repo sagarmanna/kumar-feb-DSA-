09c83453adb1fd4a86258614b901960e2b558149
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-03-03 23:14:53 +0530

    Add files via upload

---
+++ FEB_25(K.K Course)/BABY_STEP/vedio2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int x, y, z;
+    // cin>>x>>y>>z;
+    // cout<<"print this value ";
+    // cout<<x<<y<<z;
+
+
+    // 
+    int a;
+     cin>>a;
+      if(a==10){
+        cout<<"Hello World";
+
+      }else if(cond1) {
+        cout<<"Hello ";
+      }else if(cond2){
+        cout<<"Hello2";
+      }
+}
---
+++ FEB_25(K.K Course)/BABY_STEP/vedio5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // int arr[6];
+    // cout<<arr<<" ";
+int arr[5];
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    //print arr
+    for(int i =0;i<5;i++){
+        cout<<arr[i]<<"  ";
+    }
+
+}
