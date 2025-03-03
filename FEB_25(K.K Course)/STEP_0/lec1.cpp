be12ee24735a65e5a2953d5b44e77db7754bdbfa
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-03-03 23:41:14 +0530

    Add files via upload

---
+++ FEB_25(K.K Course)/STEP_0/lec1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for(int i=0;i<4;i++){
+    //     for(int i=0;i<=6;i++){
+    //         cout<<"0"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"0"<<" ";
+        }
+        cout<<endl;
+    }
+
+}
