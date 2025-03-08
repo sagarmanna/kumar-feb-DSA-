a16a2fd8ac445b9c4c4e415533f56ea6043742d6
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-03-08 14:47:32 +0530

    Add files via upload

---
+++ FEB_25(K.K Course)/STEP_1/StairCase.cpp
@@ -0,0 +1,13 @@
+void staircase(int n) {
+    
+
+    for(int i=0;i<n;i++){
+        
+    
+          int space=n-1-i;
+          for(int j =0;j<space;j++) cout<<" ";
+          for(int j=0;j<=i;j++)cout<<"#";
+          cout<<endl;
+        }
+        }
+    
