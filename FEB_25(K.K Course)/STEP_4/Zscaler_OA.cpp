248eba680faa6b49b4b196c507cccb224078c7e5
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-04-06 11:12:23 +0530

    Add files via upload

---
+++ FEB_25(K.K Course)/STEP_4/Zscaler_OA.cpp
@@ -0,0 +1,21 @@
+n
+
+b[n+1]={0}
+map<int , freq.>k
+for(i=1;i<=n;i++){
+    k[b[i]]=k[b[i]]+1;
+}
+for(auto u: k){
+    g[i].first=u
+    g[i].second=k[u]
+
+}
+
+size=g.size()
+step=0;
+for(i=size-1;i>=1;i--){
+    g[i-1].sec=g[i-1]second+g[i].second; step =step+g[i].second;
+
+    g[i].second=0;
+        }
+        
